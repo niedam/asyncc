25e7cf1656bffd84fda7fc069d4a44de5935768d
Adam <adam.rozenek@gmail.com>
2020-01-02 01:32:02 +0100

    Factorial (with SIGSEG)

---
+++ silnia.c
@@ -0,0 +1,40 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "future.h"
+
+#define POOL_SIZE 3
+#define TEST 10
+
+typedef struct factorial {
+    unsigned long long value;
+    int n;
+} factorial_t;
+
+
+static void *multiply(void *args, size_t argsz __attribute__((unused)), size_t *result_size) {
+    factorial_t *arg_fac = args;
+    *result_size = sizeof(factorial_t);
+    factorial_t *result = malloc(*result_size);
+    result->value = arg_fac->value * arg_fac->n;
+    result->n = arg_fac->n - 1;
+    //free(arg_fac);
+    return result;
+}
+
+int main() {
+    thread_pool_t pool;
+    if (thread_pool_init(&pool, POOL_SIZE) != 0) {
+        return -1;
+    }
+    int n = TEST;
+    factorial_t *comp = malloc(sizeof(factorial_t));
+    comp->n = n;
+    comp->value = 1;
+    future_t *future = malloc(sizeof(future_t));
+    async(&pool, &future[0], (callable_t){.function = multiply, .arg = comp, .argsz = sizeof(factorial_t)});
+    for (int i = 1; i < n; i++) {
+        map(&pool, &future[i], &future[i - 1], multiply);
+    }
+    factorial_t *result = await(&future[n - 1]);
+    printf("%llu", result->value);
+}
