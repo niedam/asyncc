3fea29339fe77c9709b5fa027f246e2a95390c8d
Adam <adam.rozenek@gmail.com>
2020-01-06 00:30:22 +0100

    Matrix

--- macierz.c
+++ macierz.c
@@ -5,6 +5,8 @@
 
 #define POOLSIZE 4
 
+int n, k;
+
 /**
  * Task connected with cell from matrix.
  */
@@ -13,20 +15,63 @@ typedef struct task {
     int result; /**< Result of task */
 } task_t;
 
+struct matrix_monitor {
+    pthread_mutex_t lock;
+    pthread_cond_t wait;
+    int sum;
+    int count;
+} matrix_monitor = {.lock = PTHREAD_MUTEX_INITIALIZER, .wait = PTHREAD_COND_INITIALIZER, .sum = 0, .count = 0};
 
 /**
  * Do single task and return result.
  * @param[in] args - pointer to task
  * @param[in] argsz - size of pointed memory by `args`
  * @param[out] resz - size of memory returned as result
- * @return Result of task
  */
-static void *compute(void *args, size_t argsz __attribute__((unused)), size_t *resz) {
+static void compute(void *args, size_t argsz __attribute__((unused))) {
     task_t *t = args;
     usleep(t->time);
-    *resz = sizeof(int);
-    return &t->result;
+    if (pthread_mutex_lock(&matrix_monitor.lock) != 0) {
+        exit(EXIT_FAILURE);
+    }
+    matrix_monitor.sum += t->result;
+    matrix_monitor.count++;
+    if (matrix_monitor.count == n) {
+        if (pthread_cond_broadcast(&matrix_monitor.wait) != 0) {
+            exit(EXIT_FAILURE);
+        }
+    }
+    if (pthread_mutex_unlock(&matrix_monitor.lock) != 0) {
+        exit(EXIT_FAILURE);
+    }
 }
+
+void wait_on_complete() {
+    if (pthread_mutex_lock(&matrix_monitor.lock) != 0) {
+        exit(EXIT_FAILURE);
+    }
+    while (matrix_monitor.count < n) {
+        if (pthread_cond_wait(&matrix_monitor.wait, &matrix_monitor.lock) != 0) {
+            exit(EXIT_FAILURE);
+        }
+    }
+    printf("%d ", matrix_monitor.sum);
+    if (pthread_mutex_unlock(&matrix_monitor.lock) != 0) {
+        exit(EXIT_FAILURE);
+    }
+}
+
+void reset_monitor() {
+    if (pthread_mutex_lock(&matrix_monitor.lock) != 0) {
+        exit(EXIT_FAILURE);
+    }
+    matrix_monitor.sum = 0;
+    matrix_monitor.count = 0;
+    if (pthread_mutex_unlock(&matrix_monitor.lock) != 0) {
+        exit(EXIT_FAILURE);
+    }
+}
+
 
 int main() {
     int err;
@@ -37,7 +82,6 @@ int main() {
         return(-1);
     }
     // Read size of matrix.
-    int k, n;
     scanf("%d", &k);
     scanf("%d", &n);
     // Allocate resource for computations.
@@ -46,20 +90,18 @@ int main() {
     // Assign tasks
     for (int i = 0; i < k * n; i++) {
         scanf("%d %d", &tasks[i].result, &tasks[i].time);
-        async(&pool, &computations[i], (callable_t){.function = compute, .arg = &tasks[i], .argsz = sizeof(task_t)});
     }
     // Wait for results.
     int sum;
     int i_nk = 0;
     for (int i_k = 0; i_k < k; i_k++) {
-        sum = 0;
+        reset_monitor();
         for (int i_n = 0; i_n < n; i_n++) {
-            int *val = await(&computations[i_nk]);
-            sum += *val;
+            defer(&pool, (runnable_t){.function = compute, .arg = &tasks[i_nk], .argsz = sizeof(task_t)});
             i_nk++;
         }
         // Print sum in `i_k` row.
-        printf("%d\n", sum);
+        wait_on_complete();
     }
     // Free resources.
     thread_pool_destroy(&pool);
